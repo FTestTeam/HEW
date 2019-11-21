052bdb4f023b705baa0865c7ac6d303c85afb16e
tarnyan <56944869+tarnyan@users.noreply.github.com>
2019-11-21 11:55:45 +0900

    Scene

---
+++ Result.cpp
@@ -0,0 +1,29 @@
+#include "Result.h"
+#include "texture.h"
+#include "sprite.h"
+#include "Scene.h"
+
+static int g_Result_TextureID;
+
+void Result_Init(void)
+{
+	g_Result_TextureID = Texture_SetLoadFile("Texture/Result.png", 1280, 720);
+}
+
+void Result_Uninit(void)
+{
+	
+}
+
+void Result_Update(void)
+{
+	if (Keyboard_IsPress(DIK_SPACE))
+	{
+		Scene_SetNextScene(SCENE_TITLE);
+	}
+}
+
+void Result_Draw(void)
+{
+	Sprite_Draw(g_Result_TextureID, 0, 0);
+}
---
+++ Scene.h
@@ -0,0 +1,20 @@
+#ifndef _SCENE_H_
+#define _SCENE_H_
+#include <Windows.h>
+enum SCENE
+{
+	SCENE_TITLE,
+	SCENE_GAME,
+	SCENE_RESULT,
+	SCENE_MAX,			//enumÇÃêî 
+	SCENE_END = SCENE_MAX,
+};
+
+void Scene_Init(void);
+void Scene_Uninit(void);
+void Scene_Update(void);
+void Scene_Draw(void);
+
+void Scene_SetNextScene(SCENE nextScene);
+bool Scene_Change(void);
+#endif // !_SCENE_H_
