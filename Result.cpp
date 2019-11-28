bf8ed0bdcbd906e0cc71fdd8fb6d96147c0e48a4
tarnyan <56944869+tarnyan@users.noreply.github.com>
2019-11-28 12:09:43 +0900

    score

--- Result.cpp
+++ Result.cpp
@@ -2,12 +2,17 @@
 #include "texture.h"
 #include "sprite.h"
 #include "Scene.h"
+#include "score.h"
 
 static int g_Result_TextureID;
+static int g_ResultScore = 0;
+static int g_Score1 = 0, g_Score2 = 0, g_Score3 = 0;
 
 void Result_Init(void)
 {
-	g_Result_TextureID = Texture_SetLoadFile("Texture/Result.png", 1280, 720);
+	g_Result_TextureID = Texture_SetLoadFile("Texture/Score.png", 1280, 720);
+	// 入れ替えソート
+	Result_Sort();
 }
 
 void Result_Uninit(void)
@@ -26,4 +31,31 @@ void Result_Update(void)
 void Result_Draw(void)
 {
 	Sprite_Draw(g_Result_TextureID, 0, 0);
+	//　Score_Draw(スコア,x座標,y座標,桁数,0埋め,左寄せ);
+	Score_Draw(g_ResultScore, 652, 180, 7, false, true);
+	Score_Draw(g_Score1, 652, 300, 7, false, true);
+	Score_Draw(g_Score2, 652, 360, 7, false, true);
+	Score_Draw(g_Score3, 652, 420, 7, false, true);
+}
+
+void Result_GetScore(int x)
+{
+	g_ResultScore = x;
+}
+
+void Result_Sort(void)
+{
+	if (g_ResultScore > g_Score1)
+	{
+		g_Score3 = g_Score2;
+		g_Score2 = g_Score1;
+		g_Score1 = g_ResultScore;
+	}else if (g_ResultScore > g_Score2)
+	{
+		g_Score3 = g_Score2;
+		g_Score2 = g_ResultScore;
+	}else if (g_ResultScore > g_Score3)
+	{
+		g_Score3 = g_ResultScore;
+	}
 }
