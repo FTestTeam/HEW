6001008a0dfbbdd0f18176a5d3fd48b4b24e96a3
五十嵐凌 <RyoRyo.0113@me.com>
2020-03-02 14:05:25 +0900

    ガイド追加

--- info.cpp
+++ info.cpp
@@ -1,15 +1,20 @@
 #include"texture.h"
 #include"sprite.h"
 #include"common.h"
+#include"player.h"
 
 static int g_textureID_Spin;
 static int g_anim;
 static int g_fream;
 
+static int g_textureID_Shout;
+
 void Info_Init()
 {
 	g_textureID_Spin = Texture_SetLoadFile("Asset/Texture/info.png", 768, 192);
 	g_anim = g_fream  = 0;
+
+	g_textureID_Shout = Texture_SetLoadFile("Asset/Texture/info_sakebe.png", 3840, 384);
 }
 
 void Info_UnInit()
@@ -19,16 +24,17 @@ void Info_UnInit()
 
 void Info_Update()
 {
-	if (g_anim > 5) {
+	if (g_anim > (Player_IsFly() ? 10 : 5)) {
 		g_anim = 0;
 	}
-	if (g_fream % 5 == 0) {
+	if ((g_fream % (Player_IsFly() ? 10 : 5)) == 0) {
 		g_anim++;
 	}
+	
 	g_fream++;
 }
 
 void Info_Draw()
 {
-	Sprite_Draw(g_textureID_Spin, Window_GetWidth() - 128.0f, 128, g_anim * 128, 0, 128, 192);
+	Player_IsFly() ? Sprite_Draw(g_textureID_Shout, Window_GetWidth() - 256.0f, 128.0f, g_anim * 256, 0, 256, 384) : Sprite_Draw(g_textureID_Spin, Window_GetWidth() - 128.0f, 128, g_anim * 128, 0, 128, 192);
 }
